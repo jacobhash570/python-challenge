84cf4cee19ded67db8750dd3deb92eeee6fc841c
Jacob <jacob.hash570@gmail.com>
2021-07-06 19:30:40 -0400

    Output added.

--- PyBank/main.py
+++ PyBank/main.py
@@ -58,5 +58,16 @@ print("------------------")
 print(f"Total Months: {month_count}")
 print(f"Total: {net_total}")
 print(f"Average Change: {average_change}")
-print(f"Greatest Decrease: {increase_change}")
+print(f"Greatest Increase: {increase_change}")
 print(f"Greatest Decrease {decrease_change}")
+
+output = os.path.join("analysis", "budget_data.txt")
+with open(output, "w") as output_results:
+
+    output_results.write("Financial Analysis\n")
+    output_results.write("------------------\n")
+    output_results.write(f"Total Months: {month_count}\n")
+    output_results.write(f"Total: {net_total}\n")
+    output_results.write(f"Average Change: {average_change}\n")
+    output_results.write(f"Greatest Increase: {increase_change}\n")
+    output_results.write(f"Greatest Decrease {decrease_change}\n")
