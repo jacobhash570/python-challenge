85bc4bf4372d055c4b096d1a306b3075097c24df
Jacob <jacob.hash570@gmail.com>
2021-07-06 19:05:15 -0400

    Highest/Lowest cange and print statements.

--- PyBank/main.py
+++ PyBank/main.py
@@ -46,4 +46,17 @@ with open(budget_data_csv_path, newline="") as csvfile:
 
     sum_change = sum(profit_loss_changes)
     average_change = round(sum_change/(month_count - 1))
-    print(average_change)
+    #print(average_change)
+
+    increase_change = max(profit_loss_changes)
+    #print(increase_change)
+    decrease_change = min(profit_loss_changes)
+    #print(decrease_change)
+
+print("Financial Analysis")
+print("------------------")
+print(f"Total Months: {month_count}")
+print(f"Total: {net_total}")
+print(f"Average Change: {average_change}")
+print(f"Greatest Decrease: {increase_change}")
+print(f"Greatest Decrease {decrease_change}")
