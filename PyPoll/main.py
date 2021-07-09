7c44a84ddea5bed294b5cac8b3349276f955344d
Jacob <jacob.hash570@gmail.com>
2021-07-09 10:37:45 -0400

    Total votes and sorted candidates list.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,8 @@
 import os
 import csv
 
+votes_candidates = []
+
 election_data_csv_path = os.path.join("Resources", "election_data.csv")
 
 with open(election_data_csv_path, newline="") as csvfile:
@@ -9,6 +11,12 @@ with open(election_data_csv_path, newline="") as csvfile:
 
     csv_header = next(csvfile)
     #print(csv_header)
+    
+    for row in csv_reader:
+        votes_candidates.append(row[2])
+
+    sorted_candiates = sorted(votes_candidates)
+    print(sorted_candiates)
         
 
 
